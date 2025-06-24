eaffe7e706a8d29c6435ab192f21cda33b4a8892
navin-e23 <n42140457@gmail.com>
2025-06-24 15:29:33 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-import mongoose from "mongoose";
-import { ApiError } from "../utils/ApiError.js";
+import SignInPage from "./Sign-In/SignInPage";
 
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: "ai_resume_builder",
-    });
-
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-    return conn;
-  } catch (err) {
-    throw new ApiError(500, "Database connection failed", [], err.stack);
-  }
-};
-
-export { connectDB };
+export { SignInPage}
